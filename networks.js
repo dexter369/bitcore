03a3fae8d74d5d80b341e3c6c298cb23d3fe1f66
SDCDev <sdcoin@sdcoin.co>
2014-07-19 20:57:32 +0000

    Committing genesis block

--- networks.js
+++ networks.js
@@ -11,14 +11,14 @@ exports.livenet = {
   hkeyPublicVersion: 0x0488b21e,
   hkeyPrivateVersion: 0x0488ade4,
   genesisBlock: {
-    hash: hex('c4873c989047eb6bdad13bab1bff7dd2e6822902b6587b1e096bc43d5dffa326'),
+    hash: hex('bba20c596a9c093d3239d49c2a9958dc78747b2a3faf8ac9ed074f23ca0e0000'),
     merkle_root: hex('26a3ff5d3dc46b091e7b58b6022982e6d27dff1bab3bd1da6beb4790983c87c4'),
     height: 0,
     nonce: 261836,
     version: 1,
     prev_hash: buffertools.fill(new Buffer(32), 0),
     timestamp: 1405769613,
-    bits: 0x1e0fffff,
+    bits: 504365055,
   },
   dnsSeeds: [
     'seed.sdcoin.co'
